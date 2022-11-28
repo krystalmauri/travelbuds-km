8a6ca205639e67c7c45b778a3023f64d0b509b46
Krystal M. Welch <codekrys@gmail.com>
2022-11-28 13:23:40 -0600

    Initial Commit

---
+++ TravelBuds-Laws/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelBuds-Laws
+//
+//  Created by Krys Welch on 11/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
